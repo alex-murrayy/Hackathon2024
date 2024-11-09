9cd22d878c150cad78307070d09987ccf146d6d6
Alex Murray <alexandrumurray7@gmail.com>
2024-11-09 15:34:28 -0500

    jersey color identifier

--- main.py
+++ main.py
@@ -5,17 +5,21 @@ import ball
 
 cap = cv2.VideoCapture("/Users/alex/Code/Hackathon2024/RPReplay_Final1731175138.mov")
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
 
-    frame = cv2.flip(frame, 1)
 
-    cv2.imshow('Hand Tracking', frame)
+def main():
 
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
             break
 
-cap.release()
-cv2.destroyAllWindows()
+        frame = cv2.flip(frame, 1)
+
+        cv2.imshow('Hand Tracking', frame)
+
+        if cv2.waitKey(5) & 0xFF == ord('q'):
+                break
+
+    cap.release()
+    cv2.destroyAllWindows()
--- test1.py
+++ test1.py
@@ -1,10 +1,45 @@
 import cv2
 import numpy as np
 
+def findColor(): 
+    
+    img = cv2.imread("football.jpg")
+
+    # Color contrator (can ignore)
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+
+    kernel_size = 7
+    blurred = cv2.GaussianBlur(img_hsv,(kernel_size, kernel_size),0)
+    
+    # Define range of green color in HSV
+    lower_green = np.array([40, 50, 50])
+    upper_green = np.array([80, 255, 255])
+    mask = cv2.inRange(blurred, lower_green, upper_green)
+
+    
+    # using a findContours() function 
+    contours, _ = cv2.findContours( 
+    mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    for contour in contours: 
+
+        area=cv2.contourArea(contour)
+        if area > 200 and area < 10000:
+            print(area)
+            x, y, w, h = cv2.boundingRect(contour)  # Get bounding box
+            cv2.circle(img, (int(x+w/2), int(y+h/2)), 10, (255, 0, 0), 2)
+            if y > 200:
+                cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)  # Draw rectangle
+    cv2.imshow("lol", img)
+    cv2.waitKey(0)
+
+
 def main():
     img = cv2.imread("football.jpg")
 
+    # Color contrator (can ignore)
     img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
 
     kernel_size = 7
     blurred = cv2.GaussianBlur(img_hsv,(kernel_size, kernel_size),0)
@@ -59,4 +94,4 @@ def lines():
     
     
 if __name__ == "__main__":
-    main()
+    findColor()
