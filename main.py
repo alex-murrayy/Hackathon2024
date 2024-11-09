938ea5742d4dae4f683353331cc883fae0050da0
Alex Murray <alexandrumurray7@gmail.com>
2024-11-09 11:45:18 -0500

    test

---
+++ main.py
@@ -0,0 +1,20 @@
+import cv2
+
+cap = cv2.VideoCapture(1)
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.flip(frame, 1)
+
+    pupil_detector = pupil.PupilDetection(frame)
+    pupil_detector.start_detection()
+    
+
+    if cv2.waitKey(5) & 0xFF == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
